bfa932a87f0bc6e704a544dcbd7b35bd32296db0
RORN Menghouy <menghouyrorn@gmail.com>
2024-08-20 15:43:08 +0700

    update

--- app/[locale]/(homepage)/home-page.tsx
+++ app/[locale]/(homepage)/home-page.tsx
@@ -3,10 +3,11 @@ import { CARD_DATA, DATAPAGE } from "@/constants"
 import { CardProduct } from "./_components"
 import Image from "next/image"
 import { CheckLange } from "@/shared"
-import { motion } from 'framer-motion'
+import { motion, useAnimate } from 'framer-motion'
 
 export default function HomePage() {
     const currentLang = CheckLange();
+    const controls = useAnimate()
 
     return (
         <div className="space-y-6 pt-5">
@@ -52,7 +53,7 @@ export default function HomePage() {
                 }}>
                 <div className="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-x-10 gap-y-10">
                     {CARD_DATA.map((item, index) => {
-                        return <CardProduct key={index} {...item} lable={currentLang ? item.labelkh : item.lable} des={currentLang ? item.deskh : item.des} image={`/${currentLang ? 'kh' : 'en'}${item.image}`} />
+                        return <CardProduct {...item} lable={currentLang ? item.labelkh : item.lable} des={currentLang ? item.deskh : item.des} image={`/${currentLang ? 'kh' : 'en'}${item.image}`} key={index} />
                     })}
                 </div>
             </motion.div>
