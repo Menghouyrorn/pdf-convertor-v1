0e50dfbb3d4b06cc6b1182f7eb8e366618cb848d
RORN Menghouy <menghouyrorn@gmail.com>
2025-05-08 21:59:58 +0700

    update

--- app/[locale]/(homepage)/_components/navbar/navbar-computer.tsx
+++ app/[locale]/(homepage)/_components/navbar/navbar-computer.tsx
@@ -22,41 +22,39 @@ export const NavbarComputer = () => {
     }
   }, [height]);
   return (
-    <div className={`${isFixed ? "fixed w-full z-40" : "block w-full"}`}>
-      <div className="flex h-16 pr-10 max-lg:hidden bg-gray-100 dark:bg-transparent border rounded-md items-center justify-between pl-10">
-        <div className="flex gap-x-20 items-center">
-          <Link href={"/"}>
-            <div className="flex gap-x-3 items-center">
-              <Image
-                src={currentLang ? "/kh/logo.jpg" : "/en/logo.jpg"}
-                width={55}
-                height={55}
-                className="rounded-md"
-                alt="logo"
-              />
-              <p className="font-extrabold text-sm text-[#1b1464] dark:text-white">
-                CAM PDF CONVERTER
-              </p>
-            </div>
-          </Link>
-          <div className="flex">
-            {NAVIGATION_DATA.map((item, index) => {
-              return (
-                <ButtonLink
-                  isKhmer={currentLang}
-                  active={item.active}
-                  label={currentLang ? item.labelkh : item.label}
-                  path={item.path}
-                  key={index}
-                />
-              );
-            })}
+    <div className="flex h-16 pr-10 max-lg:hidden bg-gray-100 dark:bg-transparent border rounded-md items-center justify-between pl-10">
+      <div className="flex gap-x-20 items-center">
+        <Link href={"/"}>
+          <div className="flex gap-x-3 items-center">
+            <Image
+              src={currentLang ? "/kh/logo.jpg" : "/en/logo.jpg"}
+              width={55}
+              height={55}
+              className="rounded-md"
+              alt="logo"
+            />
+            <p className="font-extrabold text-sm text-[#1b1464] dark:text-white">
+              CAM PDF CONVERTER
+            </p>
           </div>
+        </Link>
+        <div className="flex">
+          {NAVIGATION_DATA.map((item, index) => {
+            return (
+              <ButtonLink
+                isKhmer={currentLang}
+                active={item.active}
+                label={currentLang ? item.labelkh : item.label}
+                path={item.path}
+                key={index}
+              />
+            );
+          })}
         </div>
-        <div className="flex items-center gap-x-3">
-          <ButtonLanguage />
-          <ThemeButton />
-        </div>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <ButtonLanguage />
+        <ThemeButton />
       </div>
     </div>
   );
