4cb0e2880d8dd8b91eada75ad44c2169f610975f
RORN Menghouy <menghouyrorn@gmail.com>
2025-08-04 14:04:51 +0700

    add logo

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,23 +2,47 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "@/provider";
-import NextTopLoader from 'nextjs-toploader'
+import NextTopLoader from "nextjs-toploader";
 import React from "react";
 import { I18nProviderClient } from "@/locales/client";
-import { Noto_Sans_Khmer } from 'next/font/google';
+import { Noto_Sans_Khmer } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-const khmerfont = Noto_Sans_Khmer({ subsets: ['khmer'], weight: '300', style: "normal" });
+const khmerfont = Noto_Sans_Khmer({
+  subsets: ["khmer"],
+  weight: "300",
+  style: "normal",
+});
 
 export const metadata: Metadata = {
   title: "CAM-PDF-CONVERTOR",
   description: "CAM-PDF-CONVERTOR",
+  icons: {
+    icon: [
+      {
+        url: "/logo.jpg",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/logo.jpg",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
-export default async function RootLayout({ params: { locale }, children }: { params: { locale: string }, children: React.ReactElement }) {
+export default async function RootLayout({
+  params: { locale },
+  children,
+}: {
+  params: { locale: string };
+  children: React.ReactElement;
+}) {
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={locale == 'kh' ? khmerfont.className : inter.className}>
+      <body className={locale == "kh" ? khmerfont.className : inter.className}>
         <NextTopLoader color="#ef4444" showSpinner={false} />
         <I18nProviderClient locale={locale}>
           <AppProvider>{children}</AppProvider>
